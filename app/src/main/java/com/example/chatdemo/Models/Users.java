7d74df7ee348de90de33c5803561c37b04a61510
Prerna <prerna.codes@gmail.com>
2022-02-11 11:49:30 +0530

    code

---
+++ app/src/main/java/com/example/chatdemo/Models/Users.java
@@ -0,0 +1,79 @@
+package com.example.chatdemo.Models;
+
+public class Users {
+
+    String profilePicture;
+    String userName;
+    String mail;
+    String password;
+    String userId;
+    String lastMessage;
+
+    public Users(String profilePicture, String userName, String mail, String password, String userId, String lastMessage) {
+        this.profilePicture = profilePicture;
+        this.userName = userName;
+        this.mail = mail;
+        this.password = password;
+        this.userId = userId;
+        this.lastMessage = lastMessage;
+    }
+
+    public Users(){}
+
+    //signUp Constructor
+
+    public Users(String userName, String mail, String password) {
+        this.userName = userName;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+
+}
